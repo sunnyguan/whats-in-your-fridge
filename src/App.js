508876b5b37095e1ed302d1a773059a6106ff420
Sunny Guan <sunny.peng.guan@gmail.com>
2021-01-06 16:49:50 -0800

    added card demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import FoodList from './components/FoodList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit4 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FoodList />
   );
 }
 
