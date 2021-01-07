dd9338d3c867728f41540e8678b267801a19df4b
Sunny Guan <sunny.peng.guan@gmail.com>
2021-01-06 16:40:53 -0800

    changed to 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit2 <code>src/App.js</code> and save to reload.
+          Edit4 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
